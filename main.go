3aa51e1fb91589b6e85aced96d65c3ff29ce7d42
Zhangsu Wen <zhangsu.wen@ZsuWenSJC-MBP.local>
2019-03-22 16:59:01 -0700

    March 22nd Afternoon

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ func main() {
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
-	cards := newDeck()
-	cards.saveToFile("my_cards")
 
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
